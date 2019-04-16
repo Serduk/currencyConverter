3c76b7b520d1d782544adb6b138483a57af6c52e
Serhii Serdiuk <serduksergey90@gmail.com>
2019-04-16 22:00:54 +0300

    Add allert and update readme

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our 
 [online documentation](https://flutter.io/docs), which offers tutorials, 
 samples, guidance on mobile development, and a full API reference.
+
+
+- [Layouts in Flutter](https://flutter.dev/docs/development/ui/layout)
+- [Medium Flutter layouts](https://medium.com/flutter-community/flutter-layout-cheat-sheet-5363348d037e)
--- lib/CurrencyBoard.dart
+++ lib/CurrencyBoard.dart
@@ -15,6 +15,60 @@ class CurrencyBoard extends StatelessWidget {
           onPressed: () => Navigator.pop(context),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          _showDialog(context);
+        },
+      ),
     );
   }
+
+  void _showDialog(BuildContext context) {
+    String _value;
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Select currency'),
+            content: DropdownButton(
+              items: _fetchCurrency(),
+              disabledHint: Text('Select currency'),
+              onChanged: (value) {
+//                setState();
+              },
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Submit'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  List<DropdownMenuItem<String>> _fetchCurrency() {
+    List<DropdownMenuItem<String>> menuItems = List();
+
+    for (String key in _items.keys) {
+      menuItems.add(DropdownMenuItem<String>(
+        child: Text(_items[key]),
+        value: key,
+      ));
+    }
+
+    return menuItems;
+  }
+
+  final _items = {
+    '1': 'Dollar',
+    '2': 'Hrivna',
+    '3': 'Euro',
+    '4': 'Bath',
+    '5': 'Lirs'
+  };
 }
