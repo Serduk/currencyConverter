8d2ff67047e65214b506a25431df2524d1a7ed65
Serhii Serdiuk <serduksergey90@gmail.com>
2019-05-26 22:56:58 +0300

    add redux implementation

--- lib/login_screen/redux/redux_page.dart
+++ lib/login_screen/redux/redux_page.dart
@@ -12,79 +12,104 @@ class ReduxPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => StoreProvider(
-    store: store,
-    child: Scaffold(
-      appBar: AppBar(title: const Text('Redux Login Page')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            StoreConnector<AppState, String>(
-              converter: (store) {
-                store.state.login;
-                store.state.password;
-              },
-              builder: (_, text) => Text(
-                '$text',
-              ),
+        store: store,
+        child: Scaffold(
+          appBar: AppBar(title: const Text('Redux Login Page')),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                StoreConnector<AppState, String>(
+                  converter: (store) {
+                    // ignore: unnecessary_statements
+                    store.state.login;
+                    // ignore: unnecessary_statements
+                    store.state.password;
+                  },
+                  builder: (_, text) => Text(
+                        '$text',
+                      ),
+                ),
+                _LoginField(),
+                _PassField(),
+                _BtnLogin(),
+              ],
             ),
-            _LoginField(),
-            _PassField(),
-            _BtnLogin(),
-          ],
+          ),
         ),
-      ),
-    ),
-  );
+      );
 }
 
 class _LoginField extends StatelessWidget {
+  const _LoginField({Key key, this.action}) : super(key: key);
+
+  final Object action;
+
   @override
   Widget build(BuildContext context) => StoreConnector<AppState, VoidCallback>(
-    rebuildOnChange: true,
-    converter: (store) => () => store.dispatch(action),
-    builder: (_, callback) => TextFormField(
-      decoration: InputDecoration(
-        prefixIcon: Icon(Icons.email),
-        hintText: 'email@example.com',
-        labelText: 'Email',
-      ),
-      keyboardType: TextInputType.emailAddress,
-      onSaved: (value) {
+        rebuildOnChange: true,
+        converter: (store) => () => store.dispatch(action),
+        builder: (_, callback) => TextFormField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.email),
+                hintText: 'email@example.com',
+                labelText: 'Email',
+              ),
+              keyboardType: TextInputType.emailAddress,
+              validator: (msg) {
+                if (msg.toLowerCase() != 'serhii') {
+                  return 'Please, Enter valid email';
+                }
+              },
+              onSaved: (value) {
 //        action.PerformLogin();
-      },
-    ),
-  );
+              },
+            ),
+      );
 }
 
 class _PassField extends StatelessWidget {
+  const _PassField({Key key, this.action}) : super(key: key);
+
+  final Object action;
+
   @override
   Widget build(BuildContext context) => StoreConnector<AppState, VoidCallback>(
-    rebuildOnChange: true,
-    converter: (store) => () => store.dispatch(action),
-    builder: (_, callback) => TextFormField(
-    autovalidate: false,
-    obscureText: true,
-      decoration: InputDecoration(
-      labelText: 'Password',
-      prefixIcon: Icon(Icons.account_box),
-      suffixIcon: IconButton(
-      icon: Icon(Icons.lock_outline), onPressed: () {},
-      )),
-    ),
-  );
+        rebuildOnChange: true,
+        converter: (store) => () => store.dispatch(action),
+        builder: (_, callback) => TextFormField(
+              autovalidate: false,
+              obscureText: true,
+              decoration: InputDecoration(
+                  labelText: 'Password',
+                  prefixIcon: Icon(Icons.account_box),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.lock_outline),
+                    onPressed: () {},
+                  )),
+              validator: (msg) {
+                if (msg.length < 6) {
+                  return 'Your password too short';
+                }
+              },
+            ),
+      );
 }
 
 class _BtnLogin extends StatelessWidget {
+  const _BtnLogin({Key key, this.action}) : super(key: key);
+
+  final Object action;
+
   @override
   Widget build(BuildContext context) => StoreConnector<AppState, VoidCallback>(
-    rebuildOnChange: false,
-    converter: (store) => () => store.dispatch(action),
-    builder: (_, callback) => RaisedButton(
-    onPressed: callback,
-    child: Text('Login'),
-    color: Theme.of(context).primaryColor,
-    textColor: Colors.white,
-    ),
-  );
+        rebuildOnChange: false,
+        converter: (store) => () => store.dispatch(action),
+        builder: (_, callback) => RaisedButton(
+              onPressed: callback,
+              child: Text('Login'),
+              color: Theme.of(context).primaryColor,
+              textColor: Colors.white,
+            ),
+      );
 }
